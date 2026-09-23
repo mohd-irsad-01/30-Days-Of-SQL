d9f1b97eb430a8ff6fe24e853fef952ec2e053ed
mohdirsad6386-maker <mohdirsad6386@gmail.com>
2026-09-23 21:58:19 +0530

    Add Day 01 SQL practice
    Added basic SQL queries using Oracle EMP and DEPT tables.

---
+++ Day-01_Basics/basics.sql
@@ -0,0 +1,11 @@
+-- Q1. Display all employees.
+SELECT *
+FROM EMP;
+
+-- Q2. Display all dipartments.
+SELECT *
+FROM DEPT;
+
+-- Q3. Display employees name and salary.
+SELECT ENAME, SAL
+FROM EMP;
