8c2a028335cae0d5d9cd206536d038e501eae482
mohdirsad6386-maker <mohdirsad6386@gmail.com>
2026-09-24 19:49:45 +0530

    Add Day 02 SELECT practice
    Added SQL queries for SELECT statement practice using the Oracle EMP table.

---
+++ Day-02_Select/select.sql
@@ -0,0 +1,11 @@
+-- Q4 Display employee names.
+SELECT ENAME
+FROM EMP;
+
+-- Q5 Display employee name and job.
+SELECT ENAME, JOB
+FROM EMP;
+
+-- Q6 Display employee name, salary and department number.
+SELECT ENAME, SAL, DEPTNO
+FROM EMP;
