89412f3d2b622b044ee1f064172b2a1c8782ebe9
mohdirsad6386-maker <mohdirsad6386@gmail.com>
2026-09-25 07:32:49 +0530

    Add Day 03 DISTINCT practice
    Added SQL queries for DISTINCT statement practice using the Oracle EMP table.

---
+++ Day-03_DISTINCT/distinct.sql
@@ -0,0 +1,11 @@
+-- Q7. Display unique job roles. 
+SELECT DISTINCT JOB
+FROM EMP;
+
+-- Q8. Display unique department numbers. 
+SELECT DISTINCT DEPTNO
+FROM EMP;
+
+-- Q9. Display unique combinations of job and department.
+SELECT DISTINCT JOB, DEPTNO
+FROM EMP;
