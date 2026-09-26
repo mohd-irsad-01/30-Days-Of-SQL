5e4b8bddc5243393f3d8602f1f2d2a8111eaa734
mohdirsad6386-maker <mohdirsad6386@gmail.com>
2026-09-26 06:50:14 +0530

    Add Day 04 WHERE practice
    Added SQL queries for WHERE clause practice using the Oracle EMP table.

---
+++ Day-04_WHERE/where.sql
@@ -0,0 +1,19 @@
+-- Q10. Display employee whose salarary is greater than 2000.
+SELECT *
+FROM EMP
+WHERE SAL > 2000;
+
+-- Q11. Display employees whose salary less than 2000.alter
+SELECT *
+FROM EMP 
+WHERE SAL < 2000;
+
+-- Q12. Display employees whose salary equal to 3000.alter
+SELECT *
+FROM EMP
+WHERE SAL = 3000;
+
+-- Q13. Display employees from department 10.
+SELECT *
+FROM EMP 
+WHERE DEPTNO = 10;
